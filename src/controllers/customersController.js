e382537bfbcf854cb1ee2ef8afcae48e5afab6ee
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-09-30 20:42:48 -0300

    implementada funcionalidade de buscar customers por além de por cpf e id

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -2,7 +2,7 @@ import connection from '../databases/postgres.js';
 
 export async function registerCustomer(request, response) {
   const newCustomer = request.body;
-  console.log(newCustomer);
+
   try {
     await connection.query(
       `INSERT INTO customers (name, phone, cpf, birthday) VALUES
@@ -15,5 +15,29 @@ export async function registerCustomer(request, response) {
     return response.sendStatus(201);
   } catch {
     return response.status(500).send('erro ao inserir customers');
+  }
+}
+
+export async function getCustomers(request, response) {
+  const { idCustomer } = request.params;
+  const { cpf } = request.query;
+
+  const QUERY_BASIC = 'SELECT * FROM customers';
+
+  let query = QUERY_BASIC;
+
+  try {
+    if (!!cpf) {
+      query += ` WHERE customers.cpf ILIKE '${cpf}%'`;
+    } else if (!!idCustomer) {
+      query += ` WHERE customers.id = ${idCustomer}`;
+    }
+    const { rows: customer } = await connection.query(query + ';');
+
+    if (!customer) return response.sendStatus(404);
+
+    return response.status(200).send(customer);
+  } catch {
+    return response.sendStatus(500);
   }
 }
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { registerCustomer } from '../controllers/customersController.js';
+import { getCustomers, registerCustomer } from '../controllers/customersController.js';
 import ValidateNewCustomer from '../middlewares/registerCustomersMiddleware.js';
 
 const router = Router();
 
 router.post('/customers', ValidateNewCustomer, registerCustomer);
+
+router.get('/customers/:idCustomer', getCustomers);
+
+router.get('/customers', getCustomers);
 
 export default router;
