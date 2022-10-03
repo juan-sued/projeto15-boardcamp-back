2eaf4b2244a8984ddaf6aa9b9e7af99f17b3779b
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-10-03 02:30:52 -0300

    finalizando delete de aluguel

--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,10 +1,23 @@
 import { Router } from 'express';
-import validateRental from '../middlewares/rentalsMiddleware.js';
-import { getRentals, registerRental } from '../controllers/rentalsController.js';
+import {
+  validateReturnRental,
+  validateRental,
+  validateDeleteRental
+} from '../middlewares/rentalsMiddleware.js';
+import {
+  deleteRental,
+  getRentals,
+  registerRental,
+  returnRental
+} from '../controllers/rentalsController.js';
 
 const router = Router();
 
 router.post('/rentals', validateRental, registerRental);
+router.post('/rentals/:idRental/return', validateReturnRental, returnRental);
+
 router.get('/rentals', getRentals);
+
+router.delete('/rentals/:idRental', validateDeleteRental, deleteRental);
 
 export default router;
